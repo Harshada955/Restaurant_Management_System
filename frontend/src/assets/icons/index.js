3f4f9f8ff10d141f333de7fd110c41c824f7a6df
Abhishek Godse <abhigodse9556@gmail.com>
2025-03-02 13:04:51 +0530

    AG: Our Menu Page modifications

---
+++ frontend/src/assets/icons/index.js
@@ -0,0 +1,6 @@
+import vegIcon from "./veg-icon.svg";
+import nonVegIcon from "./non-veg-icon.svg";
+
+const icons = { vegIcon, nonVegIcon };
+
+export default icons;
--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,13 +1,17 @@
 import { data } from "../restApi.json";
 import PropTypes from "prop-types";
+import classes from "./Menu.module.css";
+import icons from "../assets/icons";
+import Footer from "./Footer";
 const Menu = (props) => {
   const { allMenus = [], isOurMenu = false } = props;
-  console.log(allMenus);
+  const { vegIcon, nonVegIcon } = icons;
+  const categories = [...new Set(allMenus.map((menu) => menu.category))];
   return (
     <>
-      <section className="menu" id="menu">
+      <section className={`menu ${isOurMenu && classes["our_menu"]}`} id="menu">
         <div className="container">
-          {!isOurMenu && (
+          {!isOurMenu ? (
             <>
               <div className="heading_section">
                 <h1 className="heading">POPULAR DISHES</h1>
@@ -28,47 +32,73 @@ const Menu = (props) => {
                 ))}
               </div>
             </>
+          ) : (
+            <div className={classes[""]}>
+              {categories.map((category, index) => (
+                <div className="menu_section" key={category}>
+                  <div
+                    className={`heading_section ${classes["our_menu_heading_section"]}`}
+                  >
+                    <h1 className="heading">{category}</h1>
+                  </div>
+                  <div
+                    className={
+                      index % 2 === 0
+                        ? `${classes["section_menus"]} ${classes["section_menus_reversed"]}`
+                        : classes["section_menus"]
+                    }
+                  >
+                    <div
+                      className={`${classes["section_menu_items"]} ${classes["section_menu_sub_Container"]}`}
+                    >
+                      {allMenus
+                        .filter((menu) => menu.category === category)
+                        .map((menu) => (
+                          <div
+                            className={classes["item_card"]}
+                            key={menu.menuId}
+                          >
+                            <div className={`${classes["item_type_name"]}`}>
+                              {menu.subCategory === "Veg" && (
+                                <img
+                                  src={vegIcon}
+                                  alt=""
+                                  className={classes["sub_category_icon"]}
+                                />
+                              )}
+                              {menu.subCategory === "Non-Veg" && (
+                                <img
+                                  src={nonVegIcon}
+                                  alt=""
+                                  className={classes["sub_category_icon"]}
+                                />
+                              )}
+                              <h3 className={`${classes["item_name"]}`}>
+                                {menu.item}
+                              </h3>
+                            </div>
+                            <p>
+                              {menu.halfPrice > 0 && `${menu.halfPrice}/`}
+                              {menu.fullPrice}
+                            </p>
+                          </div>
+                        ))}
+                    </div>
+                    <img
+                      className={`${classes["section_menu_image"]} ${classes["section_menu_sub_Container"]}`}
+                      src="https://media.istockphoto.com/id/1829241109/photo/enjoying-a-brunch-together.jpg?s=2048x2048&w=is&k=20&c=0UD2e_KMbkkMcx4j9ZaxGf-z1nMHxch9hi3_0BQmuAo="
+                      alt=""
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
           )}
-          <div className="heading_section">
-            <h1 className="heading">STARTERS</h1>
-          </div>
-          <div className="dishes_container">
-            {allMenus &&
-              allMenus.map(
-                (element) =>
-                  element.category === "Starters" && (
-                    <>
-                      <div className="menu-card" key={element.id}>
-                        <h3>{element.item}</h3>
-                        <p>{element.halfPrice}</p>
-                        <p>{element.fullPrice}</p>
-                        <button>{element.category}</button>
-                      </div>
-                    </>
-                  )
-              )}
-          </div>
-          <div className="heading_section">
-            <h1 className="heading">Main Course</h1>
-          </div>
-          <div className="dishes_container">
-            {allMenus &&
-              allMenus.map(
-                (element) =>
-                  element.category === "Main Course" && (
-                    <>
-                      <div className="menu-card" key={element.id}>
-                        <h3>{element.item}</h3>
-                        <p>{element.halfPrice}</p>
-                        <p>{element.fullPrice}</p>
-                        <button>{element.category}</button>
-                      </div>
-                    </>
-                  )
-              )}
-          </div>
         </div>
       </section>
+      {isOurMenu && (
+        <Footer/>
+      )}
     </>
   );
 };
